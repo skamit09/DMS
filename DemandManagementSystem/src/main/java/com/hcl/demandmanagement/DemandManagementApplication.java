e60642f1d5d9296cc84cea493431a12c3c58eed5
Amit <sk.amit09@gmail.com>
2017-09-09 09:48:11 +0530

    test

---
+++ DemandManagementSystem/src/main/java/com/hcl/demandmanagement/DemandManagementApplication.java
@@ -0,0 +1,12 @@
+package com.hcl.demandmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemandManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemandManagementApplication.class, args);
+	}
+}
